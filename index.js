3979cfe8c4c7b17f43ff29a6f5ef3d26ce516b9b
KatiFerrari <katielidasilvaferrari26@gmail.com>
2024-05-20 22:23:56 -0400

    20-05- configurações e testes iniciais com o banco de dados.

---
+++ index.js
@@ -0,0 +1,12 @@
+require ("dotenv").config();
+const conn = require ("./db/conn")
+
+conn
+ .authenticate()
+ .then(()=> {
+    console.log("Conectado ao banco de dados com sucesso!")
+ })
+
+.catch ((err)=>{
+    console.log("Erro ao conectar: " + err)
+})
