5f35f597f5ebba2d065ce93f81984773c2b4c6d8
KatiFerrari <katielidasilvaferrari26@gmail.com>
2024-05-21 14:34:41 -0400

    21-25- Implementendo o primeiro sync e criando a classe usuário.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require ("dotenv").config();
 const conn = require ("./db/conn")
 
+const Usuario = require("./models/Usuario")
+
 conn
- .authenticate()
+ .sync()
  .then(()=> {
-    console.log("Conectado ao banco de dados com sucesso!")
+    console.log("Conectado e sincronizado com o banco de dados!")
  })
 
 .catch ((err)=>{
