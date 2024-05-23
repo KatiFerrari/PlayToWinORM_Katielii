a4945524d170e57780efba125ced3dad2cf76adc
KatiFerrari <katielidasilvaferrari26@gmail.com>
2024-05-22 23:12:19 -0400

    dia22-05-formulário,rota e insert com sequelize.

--- index.js
+++ index.js
@@ -3,6 +3,34 @@ const conn = require ("./db/conn")
 
 const Usuario = require("./models/Usuario")
 
+const express = require ("express");
+const app = express();
+
+app.use(
+    express.urlencoded({
+        extend: true,
+    })
+);
+
+app.use(express.json())
+
+app.get("/usuarios/novo", (req,res)=>{
+    res.sendFile(`${__dirname}/views/formUsuario.html`);
+});
+
+app.post("/usuarios/novo", async (req, res) => {
+    const dadosUsuario = {
+        nickname: req.body.nickname,
+        nome: req.body.nome,
+    };
+
+    const usuario = await Usuario.create(dadosUsuario);
+    res.send("Uduário inserido sob o id" + usuario.id);
+})
+
+app.listen (8000, ()=>{
+    console.log("Server rodando!")
+})
 conn
  .sync()
  .then(()=> {
